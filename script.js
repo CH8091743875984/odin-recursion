0d127501f5bad5af6fb9c78b684787c33a84d076
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-09-13 09:20:04 -0500

    Added recursive fib function

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+//write iterative fib function
+
 function fibs(number) {
-  if (number === 0) {
+  if (n === 0) {
     return [0];
-  } else if (number === 1) {
+  } else if (n === 1) {
     return [0, 1];
   } else {
     const array = [0, 1];
-    console.log(number);
+    console.log(n);
     console.log(array);
 
-    while (number >= array.length + 1) {
+    while (n >= array.length + 1) {
       array.push(array.slice(-1)[0] + array.slice(-2)[0]);
     }
     return array;
+  }
+}
+
+//write recursive fib function
+
+function fibsRec(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    //call once for efficiency
+    const last = fibsRec(n - 1);
+    return last.concat(last.slice(-2)[0] + last.slice(-1)[0]);
   }
 }
