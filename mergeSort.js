3aa2e1acd4a2d36ed4540bb66cd398a169809be1
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-09-18 14:45:44 -0500

    Checkpoint

--- mergeSort.js
+++ mergeSort.js
@@ -1,14 +1,36 @@
+//Build a function mergeSort that takes in an array and returns a sorted array,
+// using a recursive merge sort methodology.
+// An input of [3, 2, 1, 13, 8, 5, 0, 1] should return [0, 1, 1, 2, 3, 5, 8, 13],
+// and an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
+
+//sort the left half of the array (assuming n >1)
+//sort the right half of the array (assuming n>1)
+
+//merge the two halves together
+
 function mergeSort(array) {
-  //sort the left half of the array (assuming n >1)
-  const midpoint = Math.trunc(array.length / 2);
-  const left = array.slice(midpoint);
-  const right = array.slice(array.length - midpoint);
-
   if (array.length === 1) {
+    console.log("Exit: " + array);
     return array;
-  } else if (array.length === 2) {
-  }
-  //sort the right half of the array (assuming n>1)
+  } else {
+    console.log("Input: " + array);
+    const midpoint = Math.trunc(array.length / 2);
+    const left = array.slice(0, midpoint);
+    const right = array.slice(midpoint);
+    console.log("Left: " + left + " Right: " + right);
 
-  //merge the two halves together
+    const newArray = [];
+    const leftValue = mergeSort(left)[0];
+    const rightValue = mergeSort(right)[0];
+
+    if (leftValue < rightValue) {
+      newArray.push(leftValue);
+      newArray.push(rightValue);
+    } else {
+      newArray.push(rightValue);
+      newArray.push(leftValue);
+    }
+    console.log("Newarray: " + newArray);
+    return newArray;
+  }
 }
